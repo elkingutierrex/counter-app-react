74a438184e680a8010ede66adf77d80fe7a4c1b3
@id26072 <elkin.gutierrez@servexternos.santander.cl>
2024-07-18 15:21:01 -0500

    feat : cambios de estilos, implementacion de bootstrap y validaciones

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -7,27 +7,45 @@ export const CounterApp = ( { value } ) => {
         setCounter( counter + 1 );
     }
 
+    const  subtractValue = () =>{
+        console.log('counter', counter);
+        counter > 0 && setCounter( counter - 1 );
+    }
+
+    const  resetValue = () =>{
+        console.log(value);
+        setCounter( value);
+    }
+
+
+
   return (
     <>
+    <img className='custom-image' src="./../src/assets/img/elkingutierrex.png" alt="logo" />
     <h1>Couter App</h1>
     <h2> { counter } </h2>
 
     <div className="div-buttons">
 
         <div>
-            <button onClick={addValue}>
+            <button disabled={counter === 0} 
+                    onClick={subtractValue}
+                    type="button" className="btn btn-secondary">
                 -1
             </button>
         </div>
         <div>
-            <button>
+            <button onClick={resetValue}
+                    type="button" className="btn btn-warning"
+                    disabled={counter === value}>
                 Reset
             </button>
         </div>
 
         <div>
 
-            <button onClick={addValue}>
+            <button onClick={addValue}
+                    type="button" className="btn btn-info">
                 +1
             </button>
         </div>
