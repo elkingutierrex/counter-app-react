c0e1a4fa5170a20f5132b3571dce89e44002bc68
@id26072 <elkin.gutierrez@servexternos.santander.cl>
2024-07-18 11:55:29 -0500

    feat : start counterApp

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 
 // import { HelloWorldApp } from './HelloWorldApp'
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
 import  './styles.css'
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title="Hola" />
+        <CounterApp value={50} />
     </React.StrictMode>
 )
