28c98239e5061d41426f9d0821708fc474eebe93
@id26072 <elkin.gutierrez@servexternos.santander.cl>
2024-07-18 14:55:53 -0500

    feat : useState Hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 export const CounterApp = ( { value } ) => {
+    const [counter, setCounter] = useState(value);
+
+    const  addValue = () =>{
+        setCounter( counter + 1 );
+    }
+
   return (
     <>
     <h1>Couter App</h1>
-    <h2> { value } </h2>
+    <h2> { counter } </h2>
+
+    <div className="div-buttons">
+
+        <div>
+            <button onClick={addValue}>
+                -1
+            </button>
+        </div>
+        <div>
+            <button>
+                Reset
+            </button>
+        </div>
+
+        <div>
+
+            <button onClick={addValue}>
+                +1
+            </button>
+        </div>
+    </div>
+    
       
     </>
   )
